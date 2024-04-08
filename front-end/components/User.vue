aa656e4cc398455393fb0b8f428929e24affa17b
Kylian2 <kylian.richard@gmail.com>
2024-04-08 18:25:20 +0200

    nouvelle base

---
+++ front-end/components/User.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+        <h1>{{ info.id }}- {{ info.username }}</h1>
+    </div>
+</template>
+<script>
+    export default{
+        props:{ //on recupere les props
+            info: Object,
+        },
+    }
+</script>
+<style>
+.card{
+    border: 2px solid red;
+    margin: 10px;
+}
+</style>
---
+++ front-end/pages/home.vue
@@ -0,0 +1,28 @@
+<template>
+    <p>Hello world</p>
+    <p>{{ users[0] }}</p>
+    <User v-for="user in users" :info="user"/>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  mounted () {
+    fetch('http://localhost:3333/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.users = data;
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log("Il y a une erreur")
+        console.log(error);
+      });
+  }
+};
+</script>
