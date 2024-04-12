f41acda885d1682f9260c938b2a956d21981b9c2
Kylian2 <kylian.richard@gmail.com>
2024-04-12 14:32:51 +0200

    date

--- front-end/components/Header.vue
+++ front-end/components/Header.vue
@@ -3,10 +3,18 @@
         <div><NuxtLink to="/"><h1 class="bg-lime-400 px-6 py-2 rounded-md">Todo Adonis</h1></NuxtLink></div>
         <div>
             <ul class="flex items-center justify-between gap-10">
-                <li>Nous somme le <span class="inline-block border-b-2 border-lime-400 border-solid mx-2">Vendredi 23 Avril</span></li>
+                <li>Nous somme le<span class="inline-block border-b-2 border-lime-400 border-solid mx-2">{{jourString + " " + jour + " " + moisString}}</span></li>
                 <li class="hover:text-lime-400 transition duration-750 "><NuxtLink to="/">À propos</NuxtLink></li>
                 <li class="border-lime-400 border-solid border-2 px-6 py-2 rounded-md hover:bg-lime-400 transition duration-750"><NuxtLink to="/create">Nouveau</NuxtLink></li>
             </ul>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+
+const date = new Date();
+let jourString = date.toLocaleString('fr-FR', { weekday: 'long' });
+const moisString = date.toLocaleString('fr-FR', { month: 'long' });
+const jour = date.getDate();
+
+</script>
